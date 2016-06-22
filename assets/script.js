fb32eedd8bac4cfe8903d5e9b34d1f01b8b24453
developer <info@frontenddaily.com>
2016-06-22 18:59:22 +0100

    Add input to change column count

--- assets/script.js
+++ assets/script.js
@@ -4,15 +4,23 @@
   var
     currentCardIndex = 0,
     cardInterval,
-    $cards = $('card-container .card'),
-    cardCount = $cards.length;
+    $cardContainer = $('.card-container'),
+    $cards = $cardContainer.find('.card'),
+    cardCount = $cards.length,
+    $columnCount = $('input[name="column-count"]');
+
+  $columnCount.val($cardContainer.css('column-count'));
+
+  $columnCount.change(function(){
+    $cardContainer.css('column-count', $columnCount.val());
+  });
 
   cardInterval = setInterval(function(){
     $cards.eq(currentCardIndex).addClass('shown');
-
+    currentCardIndex++;
     if(cardCount <= currentCardIndex){
       clearInterval(cardInterval);
     }
-  }, 1500);
+  }, 50);
 
 })(window, document, jQuery);
